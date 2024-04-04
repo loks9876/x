808eebff6566c0a01c3f547e3bdb67ec74215e2e
Raedxx1 <149210006+Raedxx1@users.noreply.github.com>
2024-04-05 00:33:08 +0300

    Update م6.js

--- plugins/م6.js
+++ plugins/م6.js
@@ -20,7 +20,7 @@ let love = `
 📍❯ ⏐ .انـطـق
 ☫ ⌟لتحويل النص الي صوت⌜
 ⦓           ⦓🏆⦔          ⦔ـ
-هذا هو قروب الدعم: https://chat.whatsapp.com/DbAGQvgPuaa2GkAYCVYEvy
+هذا هو قروب الدعم: https://chat.whatsapp.com/IVc2CWdcWem6IEwtly5oA6
 ⦓           ⦓🏆⦔          ⦔ـ
  `.trim()
 m.reply(love, null, { mentions: conn.parseMention(love) })}
