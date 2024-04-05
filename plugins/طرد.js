928a3ae56c2fb304a520dabfc4f8b84dfd2f353a
Raedxx1 <149210006+Raedxx1@users.noreply.github.com>
2024-04-05 08:18:57 +0300

    Update طرد.js

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -1,19 +1,19 @@
 
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-let kickte = `*وين شخص الي راح ينشات!*`
+let kickte = `*منشن الشخص الي عاوز تطرده!*`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*طردك فحلك روح ابكي عند امك ترجعك 😲!*`) 
+m.reply(`*تم طرد المستخدم بنجاح!✅*`) 
 
 }
 
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.command = ['kick', 'شوت'] 
+handler.command = ['طرد', 'شوت'] 
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
