ce40eab676b6ee106494f1619181b9031d84b526
Raedxx1 <149210006+Raedxx1@users.noreply.github.com>
2024-04-06 03:43:49 +0300

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -111,7 +111,8 @@ let str = `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇
           🏆
 اكتب (.م11)
 ⦓           ⦓🏆⦔          ⦔ـ
-هذا هو قروب الدعم: https://chat.whatsapp.com/DbAGQvgPuaa2GkAYCVYEvy
+هذا هو قناة اكواد البوت:https://whatsapp.com/channel/0029VaTnpSDJpe8YtqXg0u2d
+هذا هو قروب الدعم: https://chat.whatsapp.com/IVc2CWdcWem6IEwtly5oA6
 ⦓           ⦓🏆⦔          ⦔ـ
 `.trim();
 
